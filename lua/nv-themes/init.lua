e451ee1a73299617bf3d7acbcad014b690f6aa11
justanoobcoder <syaorancode@gmail.com>
2021-06-15 13:04:34 +0700

    lua/nv-themes/init.lua: first commit

---
+++ lua/nv-themes/init.lua
@@ -0,0 +1,12 @@
+-- There are 3 styles: day, night and storm
+g.tokyonight_style = "night"
+g.tokyonight_italic_comments = true
+g.tokyonight_italic_keywords = false
+g.tokyonight_italic_functions = true
+g.tokyonight_italic_variables = false
+g.tokyonight_transparent = false
+g.tokyonight_hide_inactive_statusline = false
+g.tokyonight_sidebars = {"qf", "vista_kind", "terminal", "packer"}
+g.tokyonight_dark_sidebar = true
+g.tokyonight_dark_float = true
+cmd "colorscheme tokyonight"
