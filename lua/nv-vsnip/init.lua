af69e28a563965d2f0548a1790287b9668b305e1
justanoobcoder <syaorancode@gmail.com>
2021-06-15 13:04:53 +0700

    lua/nv-vsnip/init.lua: first commit

---
+++ lua/nv-vsnip/init.lua
@@ -0,0 +1,10 @@
+g.vsnip_snippet_dir = "~/.config/nvim/snippets"
+cmd ([[
+" pmenu and vim-vsnip
+imap <expr> <Tab>   pumvisible() ? "<C-n>" : vsnip#jumpable(1)   ? "<Plug>(vsnip-jump-next)" : "<Tab>"
+imap <expr> <S-Tab> pumvisible() ? "<C-p>" : vsnip#jumpable(-1)  ? "<Plug>(vsnip-jump-prev)" : "<S-Tab>"
+
+" vim-vsnip
+smap <expr> <Tab>   vsnip#jumpable(1)  ? "<Plug>(vsnip-jump-next)" : "<Tab>"
+smap <expr> <S-Tab> vsnip#jumpable(-1) ? "<Plug>(vsnip-jump-prev)" : "<S-Tab>"
+]])
