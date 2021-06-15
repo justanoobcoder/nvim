de617a0f15114777d4b37eb44663db9d904c08be
justanoobcoder <syaorancode@gmail.com>
2021-06-15 13:04:41 +0700

    lua/nv-treesitter/init.lua: first commit

---
+++ lua/nv-treesitter/init.lua
@@ -0,0 +1,20 @@
+local ts_config = require("nvim-treesitter.configs")
+
+ts_config.setup {
+    ensure_installed = {
+        "javascript",
+        "html",
+        "css",
+        "bash",
+        "lua",
+        "json",
+        "python",
+        "cpp",
+        "c",
+        "java"
+    },
+    highlight = {
+        enable = true,
+        use_languagetree = true
+    }
+}
