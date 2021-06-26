ad8d79766a9de2c3ea59015c6f6a9667e2706fa3
justanoobcoder <syaorancode@gmail.com>
2021-06-26 21:04:38 +0700

    nv-floaterm/init.lua: first commit

---
+++ lua/nv-floaterm/init.lua
@@ -0,0 +1,8 @@
+G.floaterm_width = 0.5
+G.floaterm_height = 0.6
+G.floaterm_position = 'topright'
+
+G.floaterm_keymap_new    = '<F7>'
+G.floaterm_keymap_prev   = '<F8>'
+G.floaterm_keymap_next   = '<F9>'
+G.floaterm_keymap_toggle = '<F12>'
