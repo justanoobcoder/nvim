f3a40250a9a4d1c307fee383f3e41c49bb4aa79d
justanoobcoder <syaorancode@gmail.com>
2021-09-19 10:26:51 +0700

    nv-lspconfig: changed c/c++ server from ccls to clangd

--- lua/nv-lspconfig/init.lua
+++ lua/nv-lspconfig/init.lua
@@ -1,70 +1,50 @@
---CMD [[packadd nvim-lspconfig]]
---CMD [[packadd nvim-compe]]
-
-function on_attach(client)
-    local function map(...)
-        vim.api.nvim_buf_set_keymap(bufnr, ...)
-    end
-
-    local function buf_set_option(...)
-        vim.api.nvim_buf_set_option(bufnr, ...)
-    end
+local on_attach = function(client, bufnr)
+    local function map(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
+    local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
 
     buf_set_option("omnifunc", "v:lua.vim.lsp.omnifunc")
 
     -- Mappings.
-    local opts = {noremap = true, silent = true, normal = true}
-    map("gD", "<Cmd>lua vim.lsp.buf.declaration()<CR>", opts)
-    map("gd", "<Cmd>lua vim.lsp.buf.definition()<CR>", opts)
-    map("K", "<Cmd>lua vim.lsp.buf.hover()<CR>", opts)
-    map("gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
-    map("<C-k>", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
-    map("<space>wa", "<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>", opts)
-    map("<space>wr", "<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>", opts)
-    map("<space>wl", "<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>", opts)
-    map("<space>D", "<cmd>lua vim.lsp.buf.type_definition()<CR>", opts)
-    map("<space>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
-    map("gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
-    map("<space>e", "<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>", opts)
-    map("[d", "<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>", opts)
-    map("]d", "<cmd>lua vim.lsp.diagnostic.goto_next()<CR>", opts)
-    map("<space>q", "<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>", opts)
+    local opts = { noremap = true, silent = true }
+    map("n", "gD", "<Cmd>lua vim.lsp.buf.declaration()<CR>", opts)
+    map("n", "gd", "<Cmd>lua vim.lsp.buf.definition()<CR>", opts)
+    map("n", "K", "<Cmd>lua vim.lsp.buf.hover()<CR>", opts)
+    map("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
+    map("n", "<C-k>", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
+    map("n", "<space>wa", "<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>", opts)
+    map("n", "<space>wr", "<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>", opts)
+    map("n", "<space>wl", "<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>", opts)
+    map("n", "<space>D", "<cmd>lua vim.lsp.buf.type_definition()<CR>", opts)
+    map("n", "<space>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
+    map("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
+    map("n", "<space>x", "<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>", opts)
+    map("n", "[d", "<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>", opts)
+    map("n", "]d", "<cmd>lua vim.lsp.diagnostic.goto_next()<CR>", opts)
+    map("n", "<space>xx", "<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>", opts)
 
     -- Set some keybinds conditional on server capabilities
     if client.resolved_capabilities.document_formatting then
-        map("<space>f", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
+        map("n", "<space>f", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
     elseif client.resolved_capabilities.document_range_formatting then
-        map("<space>f", "<cmd>lua vim.lsp.buf.range_formatting()<CR>", opts)
+        map("n", "<space>f", "<cmd>lua vim.lsp.buf.range_formatting()<CR>", opts)
     end
 end
-
-local lspconf = require "lspconfig"
-local servers = {"cssls", "pyright", "bashls"}
 
 G.completion_enable_snippet = 'snippets.nvim'
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities.textDocument.completion.completionItem.snippetSupport = true
-for k, lang in pairs(servers) do
+
+local lspconf = require "lspconfig"
+local servers = { "clangd", "pyright", "bashls" }
+
+-- Load language servers
+for _, lang in ipairs(servers) do
     lspconf[lang].setup {
+        on_attach = on_attach,
         root_dir = vim.loop.cwd,
         capabilities = capabilities
     }
 end
-
--- html lsp settings
-lspconf.html.setup {
-    cmd = { "html-languageserver", "--stdio" };
-    capabilities = capabilities
-}
-
--- ccls lsp settings
-lspconf.ccls.setup {
-    cmd = {
-        'ccls',
-        '--init={"cache": {"directory": "/tmp/ccls-cache"}}'
-    };
-    capabilities = capabilities
-}
 
 -- lua lsp settings
 USER = "/home/" .. vim.fn.expand("$USER")
