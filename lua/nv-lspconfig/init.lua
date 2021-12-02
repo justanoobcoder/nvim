8a8361cbb89398e2cad41e718aa72f0430dd49da
justanoobcoder <syaorancode@gmail.com>
2021-12-02 18:57:18 +0700

    lspconfig: changed diagnostic symbols displaying code, changed prefix character preceding diagnostics text

--- lua/nv-lspconfig/init.lua
+++ lua/nv-lspconfig/init.lua
@@ -90,7 +90,16 @@ cmp.setup {
     },
 }
 
-vim.fn.sign_define("LspDiagnosticsSignError", {text = ""})
-vim.fn.sign_define("LspDiagnosticsSignWarning", {text = ""})
-vim.fn.sign_define("LspDiagnosticsSignInformation", {text = ""})
-vim.fn.sign_define("LspDiagnosticsSignHint", {text = ""})
+-- Diagnostic symbols in the sign column
+local signs = { Error = '', Warn = '', Hint = '', Info = '' }
+for type, icon in pairs(signs) do
+  local hl = "DiagnosticSign" .. type
+  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+end
+
+-- Prefix/character preceding the diagnostics' virtual text
+vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
+  virtual_text = {
+    prefix = '● ',
+  }
+})
