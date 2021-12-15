a544239250a20792727cbef7eb982e700a470940
justanoobcoder <syaorancode@gmail.com>
2021-12-15 21:30:42 +0700

    settings.lua: removed block of code about ibus issue

--- lua/settings.lua
+++ lua/settings.lua
@@ -18,30 +18,3 @@ vim.opt.relativenumber = true
 vim.opt.signcolumn = 'yes'
 vim.opt.colorcolumn = '80'
 vim.opt.inccommand = 'split'
-
--- Fix ibus issue when in normal mode
-vim.cmd [[
-function! IBusOff()
-  " Lưu engine hiện tại
-  let g:ibus_prev_engine = system('ibus engine')
-  " Chuyển sang engine tiếng Anh
-  execute 'silent !ibus engine xkb:us::eng'
-endfunction
-function! IBusOn()
-  let l:current_engine = system('ibus engine')
-  " nếu engine được set trong normal mode thì
-  " lúc vào insert mode dùng luôn engine đó
-  if l:current_engine !~? 'xkb:us::eng'
-    let g:ibus_prev_engine = l:current_engine
-  endif
-  " Khôi phục lại engine
-  execute 'silent !' . 'ibus engine ' . g:ibus_prev_engine
-endfunction
-augroup IBusHandler
-    autocmd CmdLineEnter [/?] call IBusOn()
-    autocmd CmdLineLeave [/?] call IBusOff()
-    autocmd InsertEnter * call IBusOn()
-    autocmd InsertLeave * call IBusOff()
-augroup END
-call IBusOff()
-]]
