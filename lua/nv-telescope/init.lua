693845abf24415f767f225dd4756b9fcaea09ba9
justanoobcoder <syaorancode@gmail.com>
2021-12-27 16:55:01 +0700

    nv-telescope: added new mappings, added find_nvim function

--- lua/nv-telescope/init.lua
+++ lua/nv-telescope/init.lua
@@ -27,7 +27,7 @@ require('telescope').setup{
     file_sorter =  require'telescope.sorters'.get_fuzzy_file,
     file_ignore_patterns = {},
     generic_sorter =  require'telescope.sorters'.get_generic_fuzzy_sorter,
-    path_display = { "shorten" },
+    path_display = { "smart" },
     winblend = 0,
     border = {},
     borderchars = { '─', '│', '─', '│', '╭', '╮', '╯', '╰' },
@@ -43,9 +43,24 @@ require('telescope').setup{
     }
 }
 
-vim.cmd [[
-nnoremap <leader>ff <cmd>lua require('telescope.builtin').find_files()<cr>
-nnoremap <leader>fg <cmd>lua require('telescope.builtin').live_grep()<cr>
-nnoremap <leader>fb <cmd>lua require('telescope.builtin').buffers()<cr>
-nnoremap <leader>fh <cmd>lua require('telescope.builtin').help_tags()<cr>
-]]
+local M = {}
+
+function M.find_nvim()
+    require('telescope.builtin').git_files({
+        prompt_title = '< Neovim >',
+        cwd = '~/.config/nvim',
+    })
+end
+
+local opts = { silent = true }
+map('n', '<leader>ff', ":lua require('telescope.builtin').find_files()<cr>", opts)
+map('n', '<leader>fg', ":lua require('telescope.builtin').live_grep()<cr>", opts)
+map('n', '<leader>fb', ":lua require('telescope.builtin').buffers()<cr>", opts)
+map('n', '<leader>fe', ":lua require('telescope.builtin').diagnostics()<cr>", opts)
+map('n', '<leader>fd', ":lua require('telescope.builtin').lsp_definitions()<cr>", opts)
+map('n', '<leader>gs', ":lua require('telescope.builtin').git_status()<cr>", opts)
+map('n', '<leader>gm', ":lua require('telescope.builtin').git_commits()<cr>", opts)
+map('n', '<leader>fh', ":lua require('telescope.builtin').help_tags()<cr>", opts)
+map('n', '<leader>fv', ":lua require('nv-telescope').find_nvim()<cr>", opts)
+
+return M
