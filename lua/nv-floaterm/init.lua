f35c0efa15a3c3f86d6a8c2a57dcee4da0ce7305
Nguyen Hong Hiep <syaorancode@gmail.com>
2023-12-31 13:45:51 +0700

    remove old config

--- lua/nv-floaterm/init.lua
+++
@@ -1,7 +0,0 @@
-vim.g.floaterm_width = 0.5
-vim.g.floaterm_height = 0.6
-vim.g.floaterm_position = 'topright'
-vim.g.floaterm_keymap_new    = '<F7>'
-vim.g.floaterm_keymap_prev   = '<F8>'
-vim.g.floaterm_keymap_next   = '<F9>'
-vim.g.floaterm_keymap_toggle = '<F12>'
--- lua/nv-indentline/init.lua
+++
@@ -1,7 +0,0 @@
-vim.g.indentLine_enabled = 1
-vim.g.indent_blankline_char = '▏'
-vim.g.indent_blankline_filetype_exclude = {'help', 'terminal'}
-vim.g.indent_blankline_buftype_exclude = {'terminal'}
-vim.g.indent_blankline_show_trailing_blankline_indent = false
-vim.g.indent_blankline_show_first_indent_level = false
---vim.cmd 'hi IndentBlanklineChar guifg=#2a2e36'
