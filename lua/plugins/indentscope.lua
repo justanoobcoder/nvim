09c85206db02992fda7dfc0dd7f89b1a4cf78f68
Nguyen Hong Hiep <syaorancode@gmail.com>
2024-01-11 21:42:34 +0700

    added indentscope

---
+++ lua/plugins/indentscope.lua
@@ -0,0 +1,29 @@
+return {
+  "echasnovski/mini.indentscope",
+  version = false,
+  event = { "BufReadPost", "BufWritePost", "BufNewFile" },
+  opts = {
+    symbol = "│",
+    options = { try_as_border = true },
+  },
+  init = function()
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = {
+        "help",
+        "alpha",
+        "dashboard",
+        "neo-tree",
+        "Trouble",
+        "trouble",
+        "lazy",
+        "mason",
+        "notify",
+        "toggleterm",
+        "lazyterm",
+      },
+      callback = function()
+        vim.b.miniindentscope_disable = true
+      end,
+    })
+  end,
+}
