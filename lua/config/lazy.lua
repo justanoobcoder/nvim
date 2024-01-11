6524fb1ee26a49974f47faa5dd252a1d948c9972
Nguyen Hong Hiep <syaorancode@gmail.com>
2024-01-11 21:20:41 +0700

    format

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -11,7 +11,10 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup({ { import = "plugins" }, { import = "plugins.lsp" } }, {
+require("lazy").setup({ 
+    { import = "plugins" },
+    --{ import = "plugins.lsp" }
+  }, {
   install = {
     colorscheme = { "gruvbox-material" },
   },
