0b2fca6c69472380f618dac224bce22bf995379a
Nguyen Hong Hiep <syaorancode@gmail.com>
2024-01-13 00:07:32 +0700

    added keybinds for move between windows

--- lua/config/keybinds.lua
+++ lua/config/keybinds.lua
@@ -29,6 +29,12 @@ map({ "n", "x" }, "<Down>", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent =
 map({ "n", "x" }, "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 map({ "n", "x" }, "<Up>", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 
+-- Move to window using the <ctrl> hjkl keys
+map("n", "<C-h>", "<C-w>h", { desc = "Go to left window", remap = true })
+map("n", "<C-j>", "<C-w>j", { desc = "Go to lower window", remap = true })
+map("n", "<C-k>", "<C-w>k", { desc = "Go to upper window", remap = true })
+map("n", "<C-l>", "<C-w>l", { desc = "Go to right window", remap = true })
+
 -- better indenting
 map("v", "<s-tab>", "<gv")
 map("v", "<tab>", ">gv")
