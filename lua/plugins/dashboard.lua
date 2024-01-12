d5e8999d2310029939e01cb7e7b397f3a7f78050
Nguyen Hong Hiep <syaorancode@gmail.com>
2024-01-13 02:07:52 +0700

    added open nvim config files keybind on dashboard

--- lua/plugins/dashboard.lua
+++ lua/plugins/dashboard.lua
@@ -13,7 +13,6 @@ return {
 ⠀⠀⠈⡏⠀⠀⠀⠀⠀⠀⠠⣿⣷⠀⠰⡏⢻⠉⠀⢸⡀⠀⠀⡇⠀⢹⣿⢆⢀⡀⠀⠀⠀⠀⢠⡀⠀⠀⠀⠀⠀⠟⡼⠁
 ⠀⠀⠀⣧⠀⠀⠀⠀⠀⠀⠀⠉⠉⠀⠀⠸⣸⡆⠀⠀⠁⠀⠀⡇⠀⢰⡇⠈⠉⠛⠂⠸⣿⠆⠈⠁⠀⠀⠀⠀⠀⡟⠀⠀
 ⠀⠀⠀⢸⡆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠁⠀⠀⠀⠀⠀⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⡇⠀⠀
-⠀⠀⠀⠀⢿⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡴⠢⠧⠤⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⠀⠀⠀
 ⠀⠀⠀⠀⠀⠛⠿⣦⣀⣀⣠⣤⣤⠖⠀⠀⠀⠀⠀⠐⢦⣳⣄⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⠇⠀⠀⠀
 ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⣰⡿⠃⠀⠀⠀⠀⠀⠀⢢⠀⠈⠉⢻⡗⠂⠀⠀⠀⠀⠀⠀⠙⠲⣶⠦⠤⠔⠚⠁⠀⠀⠀⠀
 ⠀⠀⠀⠀⠀⠀⠀⠀⠀⣰⣻⠁⠀⠀⠀⠀⠀⠀⠀⠈⣉⣙⠚⡟⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢦⠀⠀⠀⠀⠀⠀⠀⠀
@@ -23,7 +22,7 @@ return {
 ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠓⠒⠚⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠉⠉⠀⠀⠀⠀⠀⠀⠀⠀
     ]]
 
-    logo = string.rep("\n", 2) .. logo .. "\nDo what you love, that's freedom. Love what you do, that's happiness.\n"
+    logo = string.rep("\n", 0) .. logo .. "\nDo what you love, that's freedom. Love what you do, that's happiness.\n"
 
     local opts = {
       theme = "doom",
@@ -36,12 +35,13 @@ return {
         header = vim.split(logo, "\n"),
         -- stylua: ignore
         center = {
-          { action = "Telescope find_files",                                     desc = " Find file",       icon = " ", key = "f" },
-          { action = "Telescope oldfiles",                                       desc = " Recent files",    icon = " ", key = "r" },
-          { action = "Telescope live_grep",                                      desc = " Find text",       icon = " ", key = "g" },
-          { action = 'lua require("persistence").load()',                        desc = " Restore Session", icon = " ", key = "s" },
-          { action = "Lazy",                                                     desc = " Lazy",            icon = "󰒲 ", key = "l" },
-          { action = "qa",                                                       desc = " Quit",            icon = " ", key = "q" },
+          { action = "Telescope find_files",                                    desc = " Find file",       icon = " ", key = "f" },
+          { action = "Telescope oldfiles",                                      desc = " Recent files",    icon = " ", key = "r" },
+          { action = "Telescope live_grep",                                     desc = " Find text",       icon = " ", key = "g" },
+          { action = "lua require('util.telescope').config_files()()",          desc = " Config",          icon = " ", key = "c" },
+          { action = "lua require('persistence').load()",                       desc = " Restore Session", icon = " ", key = "s" },
+          { action = "Lazy",                                                    desc = " Lazy",            icon = "󰒲 ", key = "l" },
+          { action = "qa",                                                      desc = " Quit",            icon = " ", key = "q" },
         },
         footer = function()
           local stats = require("lazy").stats()
