92fee22cc9fff11f16cccdb1cb26b1c802a64df9
Nguyen Hong Hiep <syaorancode@gmail.com>
2024-01-13 01:57:13 +0700

    added telescope

---
+++ lua/util/telescope.lua
@@ -0,0 +1,44 @@
+local M = setmetatable({}, {
+  __call = function(m, ...)
+    return m.telescope(...)
+  end,
+})
+
+function M.telescope(builtin, opts)
+  local params = { builtin = builtin, opts = opts }
+  return function()
+    builtin = params.builtin
+    opts = params.opts
+    opts = vim.tbl_deep_extend("force", { cwd = require("util").root() }, opts or {}) --[[@as lazyvim.util.telescope.opts]]
+    if builtin == "files" then
+      if vim.loop.fs_stat((opts.cwd or vim.loop.cwd()) .. "/.git") then
+        opts.show_untracked = true
+        builtin = "git_files"
+      else
+        builtin = "find_files"
+      end
+    end
+    if opts.cwd and opts.cwd ~= vim.loop.cwd() then
+      ---@diagnostic disable-next-line: inject-field
+      opts.attach_mappings = function(_, map)
+        map("i", "<a-c>", function()
+          local action_state = require("telescope.actions.state")
+          local line = action_state.get_current_line()
+          M.telescope(
+            params.builtin,
+            vim.tbl_deep_extend("force", {}, params.opts or {}, { cwd = false, default_text = line })
+          )()
+        end)
+        return true
+      end
+    end
+
+    require("telescope.builtin")[builtin](opts)
+  end
+end
+
+function M.config_files()
+  return M.telescope("find_files", { cwd = vim.fn.stdpath("config") })
+end
+
+return M
