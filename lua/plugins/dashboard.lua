83df7e0593fc433a1b9cf84f56fa0f371e740981
Nguyen Hong Hiep <syaorancode@gmail.com>
2024-01-13 21:43:30 +0700

    added project management support

--- lua/plugins/dashboard.lua
+++ lua/plugins/dashboard.lua
@@ -34,16 +34,16 @@ return {
       },
       config = {
         header = vim.split(logo, "\n"),
-      -- stylua: ignore
-      center = {
-        { action = "Telescope find_files",                                     desc = " Find file",       icon = " ", key = "f" },
-        { action = "Telescope oldfiles",                                       desc = " Recent files",    icon = " ", key = "r" },
-        { action = "Telescope live_grep",                                      desc = " Find text",       icon = " ", key = "g" },
-        { action = [[lua require("lazyvim.util").telescope.config_files()()]], desc = " Config",          icon = " ", key = "c" },
-        { action = 'lua require("persistence").load()',                        desc = " Restore Session", icon = " ", key = "s" },
-        { action = "Lazy",                                                     desc = " Lazy",            icon = "󰒲 ", key = "l" },
-        { action = "qa",                                                       desc = " Quit",            icon = " ", key = "q" },
-      },
+        -- stylua: ignore
+        center = {
+          { action = "Telescope projects",                                        desc = " Recent Projects",  icon = " ",  key = "p" },
+          { action = "Telescope find_files",                                      desc = " Find File",        icon = " ",  key = "f" },
+          { action = "Telescope oldfiles",                                        desc = " Recent Files",     icon = " ",  key = "r" },
+          { action = "Telescope live_grep",                                       desc = " Find Uext",        icon = " ",  key = "g" },
+          { action = [[lua require("lazyvim.util").telescope.config_files()()]],  desc = " Neovim Config",    icon = " ",  key = "c" },
+          { action = 'lua require("persistence").load()',                         desc = " Restore Session",  icon = " ",  key = "s" },
+          { action = "qa",                                                        desc = " Quit",             icon = " ",  key = "q" },
+        },
         footer = function()
           local stats = require("lazy").stats()
           local ms = (math.floor(stats.startuptime * 100 + 0.5) / 100)
---
+++ lua/plugins/project.lua
@@ -0,0 +1,20 @@
+return {
+  "ahmedkhalf/project.nvim",
+  opts = {
+    manual_mode = false,
+    patterns = {
+      ".git",
+      "makefile",
+      "Makefile",
+      "pom.xml",
+      ".mvn",
+      "mvnw",
+      "mvnw.cmd",
+      "gradlew",
+      "build.gradle",
+      "package.json",
+      "go.mod",
+      "go.sum",
+    },
+  },
+}
