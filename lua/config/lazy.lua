cf3511a333bdb9393095f4cfb0ca4a659f3e96bb
Nguyen Hong Hiep <syaorancode@gmail.com>
2024-01-14 00:16:51 +0700

    moved java related files for java folder

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -16,6 +16,7 @@ require("lazy").setup({
     -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
     -- import/override with your plugins
     { import = "plugins" },
+    { import = "plugins.java" },
   },
   defaults = {
     -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
