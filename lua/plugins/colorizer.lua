4cbea31ec0c07b25bb8d5ada50da6645bedf52c3
Nguyễn Hồng Hiệp <syaorancode@gmail.com>
2024-01-18 20:30:26 +0700

    enable name color in css and html files, set colorizer mode to virtualtext

--- lua/plugins/colorizer.lua
+++ lua/plugins/colorizer.lua
@@ -1,7 +1,16 @@
 return {
   "NvChad/nvim-colorizer.lua",
   opts = {
+    filetypes = {
+      css = {
+        names = true,
+      },
+      html = {
+        names = true,
+      },
+    },
     user_default_options = {
+      mode = "virtualtext",
       names = false,
     },
   },
