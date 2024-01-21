dbc90ac4b6ed93d81b42f2cc380196c4ce19d590
Nguyễn Hồng Hiệp <syaorancode@gmail.com>
2024-01-21 21:23:08 +0700

    added oil.nvim

---
+++ lua/plugins/oil.lua
@@ -0,0 +1,8 @@
+return {
+  "stevearc/oil.nvim",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  keys = {
+    { "<leader>-", "<CMD>Oil<CR>", desc = "Open parent directory" },
+  },
+  opts = {},
+}
