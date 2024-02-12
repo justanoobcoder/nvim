a19f2dd3b30bbde8a35b26a887b65ca660a213fb
Nguyen Hong Hiep <syaorancode@gmail.com>
2024-02-12 21:31:40 +0700

    feat(copilot): change accept keymap to ctrl+right

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -7,7 +7,7 @@ return {
       auto_trigger = true,
       debounce = 75,
       keymap = {
-        accept = "<C-CR>",
+        accept = "<C-Right>",
         accept_word = false,
         accept_line = false,
         next = "<M-]>",
