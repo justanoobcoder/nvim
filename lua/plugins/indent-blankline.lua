f7422bd7f95c1b31edd712d7a2f7d68bdcafe886
Nguyen Hong Hiep <syaorancode@gmail.com>
2024-03-31 20:49:37 +0700

    feat: change indent symbol

---
+++ lua/plugins/indent-blankline.lua
@@ -0,0 +1,9 @@
+return {
+  "lukas-reineke/indent-blankline.nvim",
+  opts = {
+    indent = {
+      char = "▏",
+      tab_char = "▏",
+    },
+  },
+}
---
+++ lua/plugins/indentscope.lua
@@ -0,0 +1,6 @@
+return {
+  "echasnovski/mini.indentscope",
+  opts = {
+    symbol = "▏",
+  },
+}
